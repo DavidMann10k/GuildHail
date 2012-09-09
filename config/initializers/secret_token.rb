34ac9acfa24b8ca9a10a73850801d8b52deda1ed
David A. Mann <davidmann10k@gmail.com>
2012-09-09 18:42:48 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GuildHail::Application.config.secret_token = '26aee92cc1729f2bc2dad729f0aa64c0c8099aabe4a7cacca3cfd11a91dd95503c30623febef1cc6ac2b21801949afaf5eed867de246f321090c511a40e09cbc'
