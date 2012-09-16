222cb67f3d84c6d905c5e8bf3ee33c5631d3cca8
David A. Mann <davidmann10k@gmail.com>
2012-09-16 13:56:58 -0500

    added time_zone and added validatios to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :set_timezone
+  
+  def set_timezone
+    Time.zone = current_user.time_zone if user_signed_in?
+    Time.zone ||= "Central Time (US & Canada)"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120915032358) do
+ActiveRecord::Schema.define(:version => 20120916184318) do
 
   create_table "alliances", :force => true do |t|
     t.string   "name"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(:version => 20120915032358) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "name"
+    t.string   "time_zone"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
